3b8d36f6732ae323b0f35e86faef2061e293211d
Libraa87621 <annvps41218@gmail.com>
2024-07-15 17:53:28 +0700

    aaannnguyen
    update

--- app/src/main/java/Login_SingIn/UserActivity.java
+++ app/src/main/java/Login_SingIn/UserActivity.java
@@ -1,12 +1,10 @@
 package Login_SingIn;
 
-import android.app.Dialog;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,62 +12,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.dnmu.R;
 
 public class UserActivity extends AppCompatActivity {
+    ImageButton iconmenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        ImageButton iconMenu = findViewById(R.id.iconMenu);
-        iconMenu.setOnClickListener(new View.OnClickListener() {
+        iconmenu = findViewById(R.id.iconMenu);
+        iconmenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showCustomDialog();
+                showPopupMenu(v);
             }
         });
     }
-
-    private void showCustomDialog() {
-        Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.dialog_menu);
-
-        ListView menuListView = dialog.findViewById(R.id.menuListView);
-        String[] menuItems = getResources().getStringArray(R.array.menu_items);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menuItems);
-        menuListView.setAdapter(adapter);
-
-        menuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    public void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = menuItems[position];
-                Toast.makeText(UserActivity.this, selectedItem, Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
-
-                // Handle each item click here
-                switch (position) {
-                    case 0:
-                        // Handle "Trang chủ" click
-                        break;
-                    case 1:
-                        // Handle "Thuê xe" click
-                        break;
-                    case 2:
-                        // Handle "Vé máy bay" click
-                        break;
-                    case 3:
-                        // Handle "Liên hệ" click
-                        break;
-                    case 4:
-                        // Handle "Giới thiệu" click
-                        break;
-                    case 5:
-                        // Handle "Đăng xuất" click
-                        break;
-                }
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
             }
         });
-
-        dialog.show();
+        popupMenu.show();
     }
 }
